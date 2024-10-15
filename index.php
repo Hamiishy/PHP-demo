ab52fe13e19f75d8b50943772ee0dc7c15b939b0
Hamiishy <youngerhamish70@gmail.com>
2024-10-15 09:56:38 +0100

    int

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>water hill</title>
+</head>
+<body>
+  <?php
+    echo("000000000000<br>");
+    echo("0          0<br>");
+    echo("0   #   #  0<br>");
+    echo("0     #    0<br>");
+    echo("0   #   #  0<br>");
+    echo("000000000000<br>");
+  ?>
+  
+  <p><a href="addup.php"><p> 
+</body>
+</html>
